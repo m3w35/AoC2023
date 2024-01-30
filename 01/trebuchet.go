7ef1061dfb02d5dfa0ebb8c4980d7a5cdc099454
m3w35 <github@eatmyshorts.simplelogin.com>
2024-01-30 12:40:26 -0500

    01 part 1 complete

--- 01/trebuchet.go
+++ 01/trebuchet.go
@@ -7,20 +7,45 @@ import (
 	"fmt"
 	"m3w35/AoC2023/internal/utils"
 	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+    digits string = "0123456789"
 )
 
 var (
-    first_digit, second_digit, answer int
+    answer int
+    first_d, last_d string
+    first_i, last_i int
 )
 
+
 func calibrate(line string) {
-    // IDEA:
-    // start from line[0]; assign first digit found to first_digit.
-    // start from line[len(line)] and move backwards; assign first
-    // digit found to second_digit.
-    // answer += first_digit + second_digit
-    answer += 1
+    // Get indices of first and last digits in line.
+    first_i = strings.IndexAny(line, digits)
+    last_i = strings.LastIndexAny(line, digits)
+
+    if first_i != -1 {
+        first_d = string(line[first_i])
+    } else { // If no digit found, set first_d to 0.
+        first_d = "0"
+    }
+
+    if last_i != -1 {
+        last_d = string(line[last_i])
+    } else { // If no digit found, set last_d to 0.
+        last_d = "0"
+    }
+
+    // Join first_d to last_d and convert to int
+    s := first_d+last_d
+    c_value, err := strconv.Atoi(s)
+    utils.Check(err)
+    answer += c_value
 }
+
 
 func main() {
     file_path := utils.OpenFile(os.Args)
